1fae9bb1a3fccd007508f736060440d8f2812f15
lev <154826504+unluky3@users.noreply.github.com>
2024-03-20 12:35:51 +0200

    Update start.js

--- start.js
+++ start.js
@@ -5,8 +5,10 @@ var button_y = rootstyles.getPropertyValue('--button_y');
 var translate_button = rootstyles.getPropertyValue('--translate_button');
 var translate_main = rootstyles.getPropertyValue('--translate_main');
 var button_scale = rootstyles.getPropertyValue('--button_scale');
+var hint = rootstyles.getPropertyValue('--hint');
 
 function but() {
+  root.style.setProperty('--hint','4.5%');
   root.style.setProperty('--button_x','0%');
   root.style.setProperty('--button_y','0%');
   root.style.setProperty('--button_scale','20%');
